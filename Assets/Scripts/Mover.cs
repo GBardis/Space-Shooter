0d84bb1c1031896fd4170020df882456ed31cb26
GBardis <crisolivasavatage@hotmail.com>
2017-05-16 01:53:00 +0300

    firtst commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0.0f, 0.0f, 1.0f) * speed;
+    }
+}
